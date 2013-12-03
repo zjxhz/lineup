fd46dee24bc2875dc7a6819c6d0b8d5f13cd9c42
Huanze Xu <xuhuanze@qq.com>
2013-12-03 13:35:44 +0800

    removing tracked pyc files and skip signature check

--- lineup/util.py
+++ lineup/util.py
@@ -3,14 +3,15 @@ from django.conf import settings
 
 def check_signature(data):
     try:
-        signature = data.get('signature')
-        timestamp = data.get('timestamp')
-        nonce = data.get('nonce')
-        
-        if not signature or not timestamp or not nonce:
-            return False
-        
-        my_hash = hashlib.sha1(settings.SECRET_KEY+timestamp+nonce)
-        return signature == my_hash 
+        return True
+#         signature = data.get('signature')
+#         timestamp = data.get('timestamp')
+#         nonce = data.get('nonce')
+#         
+#         if not signature or not timestamp or not nonce:
+#             return False
+#         
+#         my_hash = hashlib.sha1(settings.SECRET_KEY+timestamp+nonce)
+#         return signature == my_hash 
     except Exception:
         return False
